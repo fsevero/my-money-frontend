65d8d43675fe15b5f18e79027a29119c94672e52
Severo <severo.fabricio@gmail.com>
2018-04-21 15:09:13 -0300

    Integrate dashboard with redux and api

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,23 +1,28 @@
 import React, {Component} from 'react'
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
+import { getSummary } from "./dashboard.actions";
 import ContentHeader from '../common/template/content.header'
 import Content from '../common/template/content'
 import Row from '../common/layout/row'
 import ValueBox from '../common/widgets/value.box'
 
 class Dashboard extends Component {
+  componentWillMount() {
+    this.props.getSummary()
+  }
+
   render() {
-    const {credits, debits} = this.props.summary
-    const consolidated = credits - debits
+    const {credit, debt} = this.props.summary
     return (
       <div>
         <ContentHeader title="Dashboard" small="v1.0" />
         <Content>
           <Row>
-            <ValueBox cols="12 4" color="green" icon="bank" value={`R$ ${credits}`} text="Créditos" />
-            <ValueBox cols="12 4" color="red" icon="credit-card" value={`R$ ${debits}`} text="Débitos" />
-            <ValueBox cols="12 4" color="blue" icon="money" value={`R$ ${consolidated}`} text="Consolidado" />
+            <ValueBox cols="12 4" color="green" icon="bank" value={`R$ ${credit}`} text="Créditos" />
+            <ValueBox cols="12 4" color="red" icon="credit-card" value={`R$ ${debt}`} text="Débitos" />
+            <ValueBox cols="12 4" color="blue" icon="money" value={`R$ ${credit - debt}`} text="Consolidado" />
           </Row>
         </Content>
       </div>
@@ -26,5 +31,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
