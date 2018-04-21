9a555e33aec202264a9be03a4ea9665438674cbd
Severo <severo.fabricio@gmail.com>
2018-04-21 18:18:20 -0300

    Adds Tabs component and subcomponents

--- src/billing-cycle/billing-cycle.jsx
+++ src/billing-cycle/billing-cycle.jsx
@@ -3,13 +3,32 @@ import React, {Component} from 'react'
 import ContentHeader from "../common/template/content.header";
 import Content from "../common/template/content";
 
+import Tabs from '../common/tabs/tabs'
+import TabsHeader from '../common/tabs/tabs.header'
+import TabHeader from '../common/tabs/tab.header'
+import TabsContent from '../common/tabs/tabs.content'
+import TabContent from '../common/tabs/tab.content'
+
 class BillingCycle extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Billing Cycles" small="v1.0" />
         <Content>
-          Billing Cycles
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label="List" icon="bars" target="tablist" />
+              <TabHeader label="Add" icon="plus" target="tabcreate" />
+              <TabHeader label="Edit" icon="pencil" target="tabupdate" />
+              <TabHeader label="Delete" icon="trash-o" target="tabdelete" />
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id="tablist">TabList</TabContent>
+              <TabContent id="tabcreate">TabCreate</TabContent>
+              <TabContent id="tabupdate">TabUpdate</TabContent>
+              <TabContent id="tabdelete">TabDelete</TabContent>
+            </TabsContent>
+          </Tabs>
         </Content>
       </div>
     )
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import dashboardReducer from '../dashboard/dashboard.reducer'
+import tabReducer from '../common/tabs/tab.reducer'
 
 const rootReducer = combineReducers({
-  dashboard: dashboardReducer
+  dashboard: dashboardReducer,
+  tab: tabReducer,
 })
 
 export default rootReducer
