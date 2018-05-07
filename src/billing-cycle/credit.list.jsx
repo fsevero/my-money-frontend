f4b2730f1e293128132581ab5c27f2fc0ddcac32
Severo <severo.fabricio@gmail.com>
2018-05-06 22:13:35 -0300

    adds new item and clone button

--- src/billing-cycle/credit.list.jsx
+++ src/billing-cycle/credit.list.jsx
@@ -1,10 +1,20 @@
-import React, { Component } from "react";
-import Grid from "../common/layout/grid";
-import { Field } from "redux-form";
+import React, { Component } from "react"
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import Grid from "../common/layout/grid"
+import { Field, arrayInsert } from "redux-form"
 
 import InputSimple from "../common/form/input.simple"
 
 class CreditList extends Component {
+
+  add(index, item = {}) {
+    if ( this.props.readOnly ) {
+      return
+    }
+
+    this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+  }
 
   renderRows() {
 
@@ -14,7 +24,14 @@ class CreditList extends Component {
       <tr key={index}>
         <td><Field name={`credits.${index}.name`} component={InputSimple} placeholder="Description" readOnly={this.props.readOnly} /></td>
         <td><Field name={`credits.${index}.value`} component={InputSimple} placeholder="Value" readOnly={this.props.readOnly} /></td>
-        <td>...</td>
+        <td>
+          <button type="button" className="btn btn-success" onClick={() => this.add(index + 1)}>
+            <i className="fa fa-plus"></i>
+          </button>
+          <button type="button" className="btn btn-warning" onClick={() => this.add(index + 1, item)}>
+            <i className="fa fa-clone"></i>
+          </button>
+        </td>
       </tr>
     ))
   }
@@ -29,7 +46,7 @@ class CreditList extends Component {
               <tr>
                 <td>Name</td>
                 <td>Value</td>
-                <td>Actions</td>
+                <td className="table-actions">Actions</td>
               </tr>
             </thead>
             <tbody>
@@ -42,4 +59,6 @@ class CreditList extends Component {
   }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert}, dispatch)
+
+export default connect(null, mapDispatchToProps)(CreditList)
